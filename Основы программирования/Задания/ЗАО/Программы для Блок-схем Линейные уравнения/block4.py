10d5de8fc37ba9439badb122964ad1db228bec12
dkalugin <dkalugin@MacBook-Pro-dkalugin.local>
2020-02-08 04:35:54 +0500

    ЛР линейные алгоритмы готова

--- Основы программирования/Задания/ЗАО/Программы для Блок-схем Линейные уравнения/block4.py
+++ Основы программирования/Задания/ЗАО/Программы для Блок-схем Линейные уравнения/block4.py
@@ -3,7 +3,6 @@ import math
 a = int(input("Введите значение a: "))
 b = int(input("Введите значение b: "))
 c = math.sqrt(pow(a, 2) + pow(b, 2))
-print(c)
 p = a + b + c
 s = 1/2 * (a * b)
 print("Периметр = " + str(int(p)))
